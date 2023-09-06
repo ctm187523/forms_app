19342e29fde032ca990575c0c3982001f2654ad7
CTM <clemente1875gmail.com>
2023-09-06 19:54:06 +0200

    fin formulario tradicional

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -46,26 +46,88 @@ class _RegisterView extends StatelessWidget {
 }
 
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+
+  //creamos un objeto GlobalKey para enlazar con el key del widget Form de abajo 
+  //para controlar el formulario basado en este key
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  
+  String username = '';
+  String email = '';
+  String password = '';
+
 
   @override
   Widget build(BuildContext context) {
     //Widget para controlar el formulario
     return Form(
+      key: _formKey,
       child: Column(
         children: [
 
           //lLamamos a la clase creada en presentation/inputs para manejar
           //los TextFormFields
-          CustomTextFormField(),
+          CustomTextFormField(
+            label: 'Nombre de usario',
+            onChange: (value) => username = value, //le damos valor a la variable creada arriba
+            validator: ( value ) {
+              if( value == null || value.isEmpty ) return 'Campo requerido';
+              if( value.trim().isEmpty ) return 'Campo requerido';
+              if( value.length < 6 ) return 'Más de 6 letras';
+              return null;
+            },
+          ),
           const SizedBox(height: 10),
-          CustomTextFormField(),
+          
+          CustomTextFormField(
+            label: 'Correo electrónico',
+            onChange: (value) => email = value, //le damos valor a la variable creada arriba
+            validator: ( value ) {
+              if( value == null || value.isEmpty ) return 'Campo requerido';
+              if( value.trim().isEmpty ) return 'Campo requerido';
+              
+              //creamos una expresion regular para validar el correo
+              final emailRegExp = RegExp(
+                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              );
+
+              //comprobamos la expresion regular con lo escrito por el usuario
+              if(!emailRegExp.hasMatch(value)) return 'No tiene formato de correo';
+              return null;
+            },
+          ),
+           const SizedBox(height: 10),
+          
+           CustomTextFormField(
+            label: 'Contraseña',
+            obscureText: true,
+            onChange: (value) => password = value, //le damos valor a la variable creada arriba
+            validator: ( value ) {
+              if( value == null || value.isEmpty ) return 'Campo requerido';
+              if( value.trim().isEmpty ) return 'Campo requerido';
+              if( value.length < 6 ) return 'Más de 6 letras';
+              return null;
+            },
+          ),
           
           const SizedBox(height: 20),
 
           FilledButton.tonalIcon( 
-            onPressed: (){}, 
+            //cuando se presiona el boton de crear usuario
+            onPressed: (){
+
+              final isValid = _formKey.currentState!.validate(); //usamos la funcion validate de arriba de Form
+              if ( !isValid ) return;
+
+              print('$username, $email, $password');
+            }, 
             icon: const Icon(Icons.save),
             label: const Text('Crear Usuario'),
           ),
--- lib/presentation/widgets/inputs/custom_text_form_field.dart
+++ lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -2,7 +2,25 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+
+  //propiedades opcionales para personalizar el TextFormField
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final Function(String)? onChange;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+
+  //constructor
+  const CustomTextFormField({
+    super.key, 
+    this.label, 
+    this.hint, 
+    this.errorMessage, 
+    this.onChange, 
+    this.validator,
+    this.obscureText = false
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -19,24 +37,32 @@ class CustomTextFormField extends StatelessWidget {
     return TextFormField(
       
       //para que no se vea lo introducido, uso en passwords
-      //obscureText: true,
+      obscureText: obscureText,
 
       //metodo que al ir escribiendo el usuario toma el texto del TextFormField
-      onChanged: (value) {
-        print('value:value');
-      },
+      onChanged: onChange, //usamos los campos del constructor
+      
       //propiedad para validar lo introducido en el TextFormField
-      validator: (value) {
-        if ( value == null) return 'Campo es requerido';
-        if ( value.isEmpty) return 'Campo es requerido';
-        if ( value.trim().isEmpty) return 'Campo es requerido'; //trim quita los espacios de delante y detras
-        return null;
-      },
+      validator: validator,
       //decoracion para mejorar esteticamente el TextFormField
       decoration: InputDecoration(
         //border cuando el input esta habilitado
         enabledBorder: border, //usamos la variable final creada arriba
         focusedBorder: border.copyWith( borderSide: BorderSide( color: colors.primary)), //la caja con el foco canbia de color
+        
+        //diseño para cuando ocurre un error al introducir datos el usuario
+        errorBorder: border.copyWith( borderSide: BorderSide( color: Colors.red.shade800)),
+        focusedErrorBorder: border.copyWith( borderSide: BorderSide( color: colors.primary)),
+        
+        isDense: true, //hace que sea mas extrecho
+        label: label != null ? Text(label!) : null,  //usamos los campos del constructor
+        hintText: hint ,//seria como el placeholder, usamos los campos del constructor
+        errorText: errorMessage, //usamos los campos del constructor
+        focusColor: colors.primary,
+        //comentamos los iconos en el TextFormField
+        //prefixIcon: Icon(Icons.supervised_user_circle_outlined, color: colors.primary), //icono al principio del TextFormField
+        //suffixIcon: Icon(Icons.supervised_user_circle_outlined, color: colors.primary), //icono al final del TextFormField
+        //icon: Icon(Icons.supervised_user_circle_outlined, color: colors.primary), //este icono lo pone fuera del TextFormLabel
       ),
     );
   }
